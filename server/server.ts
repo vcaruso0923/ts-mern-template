b2bb24b393aef61ec4c6fc6509e808d4b9e687c4
Vincent Caruso <vinnycar0923@gmail.com>
2024-02-29 14:29:18 -0800

    serve build file

--- server/server.ts
+++ server/server.ts
@@ -2,11 +2,18 @@ import './env.ts'
 import express from 'express'
 import cors from 'cors'
 import recordRoutes from './routes/record.ts'
-
+import path from 'path'
 
 const app = express()
 
 const port = process.env.PORT || 3001
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+    })
+}
 
 app.use(cors())
 app.use(express.json())
@@ -15,9 +22,9 @@ app.use(recordRoutes)
 
 // Global error handling
 app.use((err, _req, res, next) => {
-    res.status(500).send("An unexpected error occured.")
+    res.status(500).send('An unexpected error occured.')
     next()
-  })
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
