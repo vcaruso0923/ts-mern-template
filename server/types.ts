890a018d2c58b06c0371b2d9900c2311fe1de711
Vincent Caruso <vinnycar0923@gmail.com>
2025-04-10 15:58:19 -0700

    add types, not final

--- server/types.ts
+++ server/types.ts
@@ -11,3 +11,38 @@ export interface RecordResponseInterface extends RecordPayloadInterface {
     _id: ObjectId
     date: Date
 }
+
+// GET /po
+export interface GetRecordsResponse {
+    records: RecordResponseInterface[]
+}
+
+// GET /latest
+export interface GetLatestRecordsResponse {
+    records: RecordResponseInterface[]
+}
+
+// GET /:id
+export interface GetRecordByIdRequest extends Request<{id: string}> {}
+export interface GetRecordByIdResponse {
+    record?: RecordResponseInterface
+    message?: string // For "Not found" case
+}
+
+// POST /record/add
+export interface AddRecordRequest extends Request<{}, {}, RecordPayloadInterface> {}
+export interface AddRecordResponse {
+    insertedId: ObjectId
+}
+
+// PATCH /comment/:id
+export interface AddCommentRequest extends Request<{id: string}, {}, {comment: string}> {}
+export interface AddCommentResponse {
+    modifiedCount: number
+}
+
+// DELETE /:id
+export interface DeleteRecordRequest extends Request<{id: string}> {}
+export interface DeleteRecordResponse {
+    deletedCount: number
+}
